7ad5a8fbea61a4675965be7c94f50cbae8227b6b
Kirk Elliott <kirk@addthis.com>
2016-04-23 14:45:45 -0400

    added proper framework stuff

---
+++ NotRegex/NotRegex.h
@@ -0,0 +1,19 @@
+//
+//  NotRegex.h
+//  NotRegex
+//
+//  Created by Kirk Elliott on 4/23/16.
+//  Copyright © 2016 dmvjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotRegex.
+FOUNDATION_EXPORT double NotRegexVersionNumber;
+
+//! Project version string for NotRegex.
+FOUNDATION_EXPORT const unsigned char NotRegexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotRegex/PublicHeader.h>
+
+
